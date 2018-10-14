5686c46be4aeb4251add75727c8cefe8f641ce50
jgcaballero <jgcaballero0@gmail.com>
2018-10-14 00:32:06 -0600

    #2 semi working #4 done

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -1,16 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 10 19:31:37 2018
-
-@author: josecaballero
-"""
-
 import numpy as np
 import cv2
 from PIL import Image
+import time
 import pylab as plt
-#plt.switch_backend('TKAgg')
+plt.switch_backend('TKAgg')
 
 #✔️
 def real_value_indexing(): #✔️
@@ -64,21 +57,34 @@ def transform(H,fp):
         t = np.dot(H,fp)
     return t[:2]
   
-def demo():
+def forward_mapping():
     img = cv2.imread('images/banner_small.jpg',1)
     triangle1, triangle2 = triangle(img)
-    #cv2.imshow('image',triangle1)
-    #cv2.imshow('image2',triangle2)
+    
+    im1 = np.array(triangle1, dtype=np.uint8)
+    plt.figure(3)
+    plt.imshow(triangle1)
+    plt.show()
+    
+    x2 = [0,0,im1.shape[0]-1]
+    y2 = [0,im1.shape[1]-1,0]
+    fp2 = np.vstack((x2,y2))
+    
+    im1 = np.fliplr(im1)
+    im1 = np.flipud(im1)
     
     im2 = np.array(triangle2, dtype=np.uint8)
     plt.figure(1)
     plt.imshow(triangle2)
     plt.show()
-    
+        
     source_im = np.array(Image.open('images/tennis.jpg'), dtype=np.uint8)
     plt.figure(2)
     plt.imshow(source_im)
     plt.show()
+    
+    max_row = source_im.shape[0]-1
+    max_col = source_im.shape[1]-1
     
     x = [0,0,im2.shape[0]-1]
     y = [0,im2.shape[1]-1,0]
@@ -87,8 +93,14 @@ def demo():
     #print("Click destination points, top-left, top-tight, and bottom-left corners")
     tp = np.asarray(plt.ginput(n=3), dtype=np.float).T
     tp = tp[[1,0],:]
-    print(fp)
-    print(tp)
+    print('fp', fp)
+    print('tp', tp)
+    
+    #print("Click destination points, top-right, bottorm-left, and bottom-right corners")
+    tp2 = np.asarray(plt.ginput(n=3), dtype=np.float).T
+    tp2 = tp2[[1,0],:]
+    print('fp', fp2)
+    print('tp', tp2)
     
     #Using pseudoinverse
     # Generating homogeneous coordinates
@@ -96,25 +108,137 @@ def demo():
     tph = np.vstack((tp,np.ones(tp.shape[1])))
     H = np.dot(tph,np.linalg.pinv(fph))
     
-    print((transform(H,fp)+.5).astype(np.int))
+    fph2 = np.vstack((fp2,np.ones(fp2.shape[1])))
+    tph2 = np.vstack((tp2,np.ones(tp2.shape[1])))
+    H2 = np.dot(tph2,np.linalg.pinv(fph2))
     
+    print('wat', (transform(H,fp)+.5).astype(np.int))
+    
+    Cs = (transform(H,fp)+.5).astype(np.int)
+    Cs[Cs<0] = 0
+    Cs[0,Cs[0]>max_row] = max_row             
+    Cs[1,Cs[1]>max_col] = max_col    
+    
+    Cs2 = (transform(H2,fp2)+.5).astype(np.int)
+    Cs2[Cs2<0] = 0
+    Cs2[0,Cs2[0]>max_row] = max_row             
+    Cs2[1,Cs2[1]>max_col] = max_col    
+        
     #Generating pixel coordinate locations
     ind = np.arange(im2.shape[0]*im2.shape[1])
     row_vect = ind//im2.shape[1]
     col_vect = ind%im2.shape[1]
     coords = np.vstack((row_vect,col_vect))
+    print('coords', coords)
     
     new_coords = transform(H,coords).astype(np.int)
+    new_coords[new_coords<0] = 255
+    new_coords[0,new_coords[0]>max_row] = max_row             
+    new_coords[1,new_coords[1]>max_col] = max_col    
+    print('new_coords', new_coords)
     target_im = source_im
-    target_im[new_coords[0],new_coords[1],:] = im2[coords[0],coords[1],:]
+    
+    print('---------------------------')
+    for y in range(im2.shape[0]):
+        for x in range(im2.shape[1]):
+            if(np.dot(im2[y,x],im2[y,x]) == 0 ):
+                im2 = np.delete(im2, im2[y,x])
+    print('---------------------------')
 
+    print('---------------------------')
+    for y in range(im1.shape[0]):
+        for x in range(im1.shape[1]):
+            if(np.dot(im1[y,x],im1[y,x]) == 0 ):
+                im1 = np.delete(im1, im1[y,x])
+    print('---------------------------')
+    
+    
+    target_im[new_coords[0],new_coords[1],:] = im2[coords[0],coords[1],:]
+    
+    #Generating pixel coordinate locations
+    ind2 = np.arange(im1.shape[0]*im1.shape[1])
+    row_vect2 = ind2//im2.shape[1]
+    col_vect2 = ind2%im2.shape[1]
+    coords2 = np.vstack((row_vect2,col_vect2))
+    print('coords', coords2)
+    
+    new_coords2 = transform(H2,coords2).astype(np.int)
+    target_im = source_im
+    target_im[new_coords2[0],new_coords2[1],:] = im1[coords2[0],coords2[1],:]
+    print('PLS', im1[coords2[0],coords2[1],:])
+    print('PLS', im1[coords2[0]])
+    
+        
+    cv2.imshow('image',target_im)
+
+    
     plt.figure(3)
     plt.imshow(target_im)
     plt.show()
-
     
+def single_point_warp():
+    
+    source_im = np.array(Image.open('images/opencv.jpg'), dtype=np.uint8)
+    plt.figure(1)
+    plt.imshow(source_im)
+    plt.show()
+    
+    print("Click source and destination of warp point")
+    p = np.asarray(plt.ginput(n=2), dtype=np.float32)
+    print(p)
+    print(p[0]-p[1])
+    plt.plot(p[:,0], p[:,1], color="blue")
+    plt.plot(p[0][0], p[0][1],marker='x', markersize=3, color="red")
+    plt.plot(p[1][0], p[1][1],marker='x', markersize=3, color="red")
+    #plt.show()
+    start = time.time()
+    
+    #Generate pixels coordinates in the destination image       
+    dest_im = np.zeros(source_im.shape, dtype=np.uint8)                 
+    max_row = source_im.shape[0]-1
+    max_col = source_im.shape[1]-1
+    dest_rows = dest_im.shape[0]
+    dest_cols = dest_im.shape[1]
+    
+    #Painting outline of source image black, so out of bounds pixels can be painted black  
+    source_im[0]=0
+    source_im[max_row]=0         
+    source_im[:,0]=0
+    source_im[:,max_col]=0 
+             
+    #Generate pixel coordinates in the destination image         
+    ind = np.arange(dest_rows*dest_cols )
+    row_vect = ind//dest_cols
+    col_vect = ind%dest_cols
+    coords = np.vstack((row_vect,col_vect))
+    
+    #Computing pixel weights, pixels close to p[1] will have higher weights    
+    dist = np.sqrt(np.square(p[1][1] - row_vect) + np.square(p[1][0] - col_vect))
+    weight = np.exp(-dist/100)         #Constant needs to be tweaked depending on image size
+    
+    #Computing pixel weights, pixels close to p[1] will have higher weights    
+    source_coords = np.zeros(coords.shape, dtype=np.int)
+    disp_r = (weight*(p[0][1]-p[1][1])).astype(int)
+    disp_c = (weight*(p[0][0]-p[1][0])).astype(int)
+    source_coords[0] = coords[0] + disp_r
+    source_coords[1] = coords[1] + disp_c
+                 
+    #Fixing out-of-bounds coordinates               
+    source_coords[source_coords<0] = 0
+    source_coords[0,source_coords[0]>max_row] = max_row             
+    source_coords[1,source_coords[1]>max_col] = max_col      
+          
+    dest_im = source_im[source_coords[0],source_coords[1],:].reshape(dest_rows,dest_cols,3)
+    
+    cv2.imshow('image',dest_im)
+    
+    elapsed_time = time.time()-start
+    print('Elapsed time: {0:.2f} '.format(elapsed_time))  
+    
+while True:
+    single_point_warp()
 
-demo()
+#forward_mapping()
 
 k = cv2.waitKey(0)
 if k == 27:         # wait for ESC key to exit
