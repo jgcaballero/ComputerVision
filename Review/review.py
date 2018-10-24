a53d41a14b9833a5ebd786ed34671bf2d227fa3d
Jose Caballero <jcaballero@steeleconsult.net>
2018-10-24 12:56:25 -0600

    final

--- Review/review.py
+++ Review/review.py
@@ -1,6 +1,9 @@
 import numpy as np
 import cv2
 from PIL import Image
+import pylab as plt
+import math
+
 
 #1A
 def grayscale():
@@ -83,19 +86,75 @@ def convolve(image,kernel):
 #1G    
 def gauss_filter():
     image = cv2.imread('images/cat.jpg')
-    blur = cv2.GaussianBlur(image,(5,5),0)
-    cv2.imshow('crop',image) 
-    cv2.imshow('boxd',blur) 
+    gauss_blur = np.array([[1,2,1], [2,4,2], [1,2,1]])/16
+    dst = cv2.filter2D(image,-1,gauss_blur)
+    res = np.hstack((image,dst))
+    cv2.imshow('result',res) 
+  
 
 #1H
 def box_filter():
     image = cv2.imread('images/cat.jpg',0)
     box_size = 10
     kernel = np.ones((box_size,box_size))/(box_size*box_size)
-    gray_frame_f = np.abs(cv2.filter2D(image,-1,kernel))
-    cv2.imshow('crop',image) 
-    cv2.imshow('boxd',gray_frame_f) 
+    box = np.array([[1,1,1], [1,1,1], [1,1,1]])/9
+    gray_frame_f = np.abs(cv2.filter2D(image,-1,box))
+    #cv2.imshow('crop',image) 
+    #cv2.imshow('boxd',gray_frame_f) 
+    res = np.hstack((image,gray_frame_f))
+    cv2.imshow('result',res) 
+    
+def threshold():
+    gray_frame = cv2.imread('images/ny.jpg',0)
+    thr_frame = (gray_frame>np.median(gray_frame)).astype(np.uint8)*255
+    res = np.hstack((gray_frame,thr_frame))
+    cv2.imshow('frame',res) 
+    
+def threshold2():
+    img = cv2.imread('images/ny.jpg',0)
+    
+    for y in range(img.shape[0]):
+        for x in range(img.shape[1]):
+            if(img[y,x] > 128):
+                img[y,x] = 255
+            else:
+                img[y,x] = 0
+            
+    cv2.imshow('frame',img) 
 
+                        
+
+def edges():
+    image = cv2.imread('images/ny.jpg')
+    kernel_v = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
+    kernel_h = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
+    #np.array([[-1,-1],[1,1]])
+    edges = np.abs(cv2.filter2D(image,-1,kernel_v))+np.abs(cv2.filter2D(image,-1,kernel_h))
+    res = np.hstack((image,edges))
+    cv2.imshow('edges',res) 
+
+def hist2():
+    img = cv2.imread('images/cat.jpg',0)
+    flat = img.flatten() 
+    n = 4
+    bar_hist = np.zeros(n)
+    raange = 255 // n
+
+    for x in range(len(flat)):
+        px = flat[x]
+        for i in range(1,n+1):
+            if(px <= raange*i):
+                bar_hist[i-1] += 1
+                break
+                
+    print(bar_hist)
+    
+def hist3():
+    I = cv2.imread('images/cat.jpg',0)
+    n = 4
+    plt.hist(I.ravel(),n,[0,256])
+    plt.show()
+    
 #Number 4    
 def is_same_picture():
     img1 = np.array(Image.open('images/cat.jpg'), dtype=np.uint8)
@@ -105,8 +164,127 @@ def is_same_picture():
         print('identical')
     else:
         print('NOT identical')
+        
+#Generate pixel coordinates in the destination image         
+def coord_mat(rows,cols):
+    ind = np.arange(rows*cols)
+    row_mat  = (ind//cols).reshape((rows,cols))
+    col_mat  = (ind%cols).reshape((rows,cols))
+    return row_mat, col_mat
 
-half_size()
+def swap_RB(img):
+    img =  img[:,:,[2,1,0]]
+        
+#6A
+def array_warp(I,W):
+    rows = I.shape[0]
+    cols = I.shape[1]
+    row_mat, col_mat = coord_mat(rows,cols)
+    row_mat == row_mat + W[:,:,0]
+    col_mat == col_mat + W[:,:,1]
+
+#6B
+def point_warp(rows,cols, p,q,k):
+    row_mat, col_mat = coord_mat(rows,cols)
+    W = np.zeros((rows,col,2))
+    dist_r = row_mat - q[0]
+    dist_c = col_mat - q[1]
+    dist = -np.sqrt(dist_r*dist_r + dist_c*dist_c)/k
+    W[:,:,0] = np.exp(dist)*(p[0] - q[0])
+    W[:,:,1] = np.exp(dist)*(p[1] - q[1])
+    W = (W+0.5).astype(np.int)
+    return W
+
+#Number 7
+def knn(Xtrain, Ytrain, k, Xtest):
+    Ytest = np.zeros((Ytrain.shape[0],Xtest.shape[1]))
+    for i in range(Xtest.shape[1]):
+        diff = Xtrain - Xtest[i]
+        diff = diff * diff
+        diff = np.sum(diff, axis = 0)
+        diff = np.sqrt(diff)
+        n = np.argsort(diff)[:k]
+        w = 1/diff[n]  
+        w = w/np.sum(w)
+        Ytest[:,i] = Ytrain[:,n]*w
+    return Ytest
+
+def integral():
+    I = np.ones((3,3))
+    print(I)
+    S = np.zeros((I.shape[0],I.shape[1]))
+    S[0:,0:] = np.cumsum(I,axis =1)
+    S = np.cumsum(S,axis = 0)
+    print('----------------')
+    print(S)
+    return I, S
+
+def sum_region(I,r0,c0,r1,c1):
+    return np.sum(I[r0:r1+1,c0:c1+1])
+
+def sum_region_integral(S,r0,c0,r1,c1):
+    return S[r1+1,c1+1] - S[r1+1,c0] - S[r0,c1+1] + S[r0,c0]
+
+def hor_grad(I):
+    kernel = np.ones((1,2))
+    kernel[0,0]=-1
+    return cv2.filter2D(I,-1,kernel,borderType=cv2.BORDER_REPLICATE)
+
+def ver_grad(I):    
+    kernel = np.ones((2,1)) 
+    kernel[0,0]=-1
+    return cv2.filter2D(I,-1,kernel,borderType=cv2.BORDER_REPLICATE)
+
+def grad_mag(vg,hg):    
+    return np.sqrt(vg*vg+hg*hg)
+
+def grad_angle(vg,hg): 
+    t = np.arctan2(vg,hg)*180/math.pi
+    t[t<0] = t[t<0] + 360
+    return t
+
+def hog(gm,ga,bars): 
+    hist = np.zeros(bars)
+    assigned_bar = (ga//(360/bars)+.5).astype(np.int)
+    for b in range(bars):
+        hist[b] = np.sum(gm[assigned_bar==b])
+
+def hist_of_grad():
+    I = np.random.randint(10, size=24).reshape((4,6)).astype(np.float)
+    
+    #calculate horizontal gradient
+    hg = hor_grad(I)
+    
+    #calculate vertical gradient
+    vg = ver_grad(I)
+    
+    print('hg', hg)
+    print('==========================')
+    print('vg', vg)
+    
+    #calculate magniture of gradients (square root)
+    gm = grad_mag(vg,hg)
+    
+    #gettubg angle of gradients with some obscure formula
+    ga = grad_angle(vg,hg)
+    
+    print(gm)
+    print('==========================')
+    print(ga)
+    
+    
+    h = hog(gm,ga,8)
+    print(h)
+    print('sum', np.sum(h))
+    print('sum2', np.sum(gm))
+
+I, S = integral()
+
+print(sum_region(I,0,0,2,2))
+print(sum_region_integral(S,0,0,2,2))
+
+hist_of_grad()
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
