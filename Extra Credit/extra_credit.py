714623bb37399ce58d0369cc033613749dc0114b
jgcaballero <jgcaballero0@gmail.com>
2018-10-30 01:49:23 -0600

    Final fixes before mimish

--- Extra Credit/extra_credit.py
+++ Extra Credit/extra_credit.py
@@ -67,14 +67,27 @@ def enlarge():
     
 #e)
 def edges():
-    image = cv2.imread('images/quijote_lr.jpg')
-    kernel_t = np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
-    edgez =cv2.filter2D(image,-1,kernel_t)
+    image = cv2.imread('images/quijote_lr.jpg',0)
+    kernel_v = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
+    kernel_h = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
+    gray_frame_f = np.abs(cv2.filter2D(image,-1,kernel_v))+np.abs(cv2.filter2D(image,-1,kernel_h))
     
-    res = np.hstack((image,edgez))
+#    for y in range(edgez.shape[0]):
+#        for x in range(edgez.shape[1]):
+#            if(edgez[y,x] > 128):
+#                edgez[y,x] = 255
+#            else:
+#                edgez[y,x] = 0
+            
+    
+    res = np.hstack((image,gray_frame_f))
     cv2.imshow('edges',res) 
     
+#2)
 def blue_bg():
+    #SOURCE: https://docs.opencv.org/master/df/d9d/tutorial_py_colorspaces.html#gsc.tab=0
+    #https://stackoverflow.com/questions/38357141/identifying-green-circles-from-this-image/38357999#38357999
+    
     image = cv2.imread('images/quijote_lr.jpg')
     windmill = cv2.imread('images/windmill.jpg')
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -86,32 +99,26 @@ def blue_bg():
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
     res = cv2.bitwise_and(image,image, mask= mask)  
     
-    print('res',res.shape)
-    print('wind',windmill.shape)
-    
+    print(hsv)
 
     for y in range(windmill.shape[0],0,-1):
         for x in range(windmill.shape[1],0,-1):
-           if(np.all(res[y-101,x-184] == black)):
-               windmill[y-1,x-1] = image[y-101,x-184]
+            if(x != 183 and y != 100):
+               if(np.all(res[y-101,x-184] == black)): 
+                   windmill[y-1,x-1] = image[y-101,x-184]
 
-                   
-               
-           
+            else:
+                break
+        if(y == 100):
+            break
 
-    cv2.imshow('frame',image)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
+#    cv2.imshow('frame',image)
+#    cv2.imshow('mask',mask)
+#    cv2.imshow('res',res)
     cv2.imshow('res',windmill)
 
 
-
-
-
-    
-
-    
-blue_bg()
+enlarge()
     
 cv2.waitKey(0)
 cv2.destroyAllWindows()
