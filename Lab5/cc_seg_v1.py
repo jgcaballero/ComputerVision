e3abc92b7f038e67f8209f80e73dc45d1bdfe9ad
jgcaballero <jgcaballero0@gmail.com>
2018-11-29 23:16:23 -0700

    part 2 probably done

--- Lab5/cc_seg_v1.py
+++ Lab5/cc_seg_v1.py
@@ -30,9 +30,9 @@ def union(i,j,thr):
             sum_pixels[ri,:]+=sum_pixels[rj,:]
             sum_pixels[rj,:]=0
                   
-def initialize(I):
-    rows = I.shape[0]
-    cols = I.shape[1]   
+def initialize(I,rows,cols):
+#    rows = I.shape[0]
+#    cols = I.shape[1]   
     S=np.zeros(rows*cols).astype(np.int)-1
     count=np.ones(rows*cols).astype(np.int)       
     sum_pixels = np.copy(I).reshape(rows*cols,3)      
@@ -41,27 +41,41 @@ def initialize(I):
 def connected_components_segmentation(I,thr):
     rows = I.shape[0]
     cols = I.shape[1]   
+    
     for p in range(S.shape[0]):
         if p%cols < cols-1:  # If p is not in the last column
             union(p,p+1,thr) # p+1 is the pixel to the right of p  
         if p//cols < rows-1: # If p is not in the last row   
             union(p,p+cols,thr) # p+cols is the pixel to below p  
+        if( p%cols < cols-1 and p//cols < rows-1):
+            union(p, p+(1+cols), thr)
 
-thr=0.2
-I  =  (cv2.imread('quijote_lr.jpg',1)/255)
-#I  =  (cv2.imread('capetown.jpg',1)/255)
+start = time.time()
+
+thr=0.1
+
+#I  =  (cv2.imread('images/umbrela.jpg',1)/255)
+#I  =  (cv2.imread('images/shapes.jpg',1)/255)
+#I  =  (cv2.imread('images/shapes.jpg',1)/255)
+I  =  (cv2.imread('images/eyeshadow.jpg',1)/255)
 #I  =  (cv2.imread('mayon.jpg',1)/255)
+
+
+
 cv2.imshow('Original',I)
 
-rows = I.shape[0]
-cols = I.shape[1]   
-S, count, sum_pixels = initialize(I)
+cols = I.shape[1] 
+rows = I.shape[0]  
+S, count, sum_pixels = initialize(I,rows,cols)
 connected_components_segmentation(I,thr)
+
 
 print('Regions found: ',np.sum(S==-1))
 print('Size 1 regions found: ',np.sum(count==1))
 
 rand_cm = np.random.random_sample((rows*cols, 3))
+
+#zeroes = np.zeros((rows,cols,3))
 seg_im_mean = np.zeros((rows,cols,3))
 seg_im_rand = np.zeros((rows,cols,3))
 for r in range(rows-1):
@@ -69,6 +83,15 @@ for r in range(rows-1):
         f = find(r*cols+c)
         seg_im_mean[r,c,:] = sum_pixels[f,:]/count[f]
         seg_im_rand[r,c,:] = rand_cm[f,:]
-            
+                
 cv2.imshow('Segmentation 1 - using mean colors',seg_im_mean)
 cv2.imshow('Segmentation 2 - using random colors',seg_im_rand)
+
+elapsed_time = time.time()-start
+print('Time it took to process img', elapsed_time)
+
+k = cv2.waitKey(0)
+if k == 27:         # wait for ESC key to exit
+    cv2.destroyAllWindows()
+elif k == ord('s'): # wait for 's' key to save and exit
+    cv2.destroyAllWindows()   
